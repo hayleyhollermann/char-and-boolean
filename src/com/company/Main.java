30f86b93dd248b6255484b5ecabcf43141ecc763
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-02-26 13:19:26 -0600

    prints char variables / symbols

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,10 @@ public class Main {
         char myUnicodeChar = '\u0044'; // unicode for letter D
         System.out.println("myChar: " + myChar);
         System.out.println("myUnicodeChar: " + myUnicodeChar); // prints D
+        char myCopyrightChar = '\u00A9';
+        System.out.println("myCopyrightChar: " + myCopyrightChar); // prints copyright symbol
+
+        
 
     }
 }
